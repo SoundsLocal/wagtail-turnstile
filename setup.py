5e819b72ce89f6df1d94f545e26b03d2f576ae65
Mark Steadman <mark@origin.fm>
2025-04-21 12:13:42 +0100

    Plaves version info in turnstile package

--- setup.py
+++ setup.py
@@ -1,9 +1,26 @@
 from setuptools import setup, find_packages
+import os
+import re
+
+
+def get_version(*file_paths):
+    """Retrieves the version from turnstile/__init__.py"""
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    version_file = open(filename).read()
+    version_match = re.search(
+        r"^__version__ = ['\"]([^'\"]*)['\"]", version_file,
+        re.M
+    )
+
+    if version_match:
+        return version_match.group(1)
+
+    raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name="wagtail-turnstile",
-    version="0.3.0",
+    version=get_version("turnstile", "__init__.py"),
     description="Cloudflare Turnstile integration for Wagtail forms",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- turnstile/__init__.py
+++ turnstile/__init__.py
@@ -1,1 +1,2 @@
 default_app_config = "turnstyle.apps.TurnstileConfig"
+__version__ = "0.3.1"
