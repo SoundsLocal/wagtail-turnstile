3e502cc491df747d1b6b739173afb180b4a90638
Mark Steadman <mark@origin.fm>
2025-04-21 12:05:01 +0100

    Adds an “appearance” option to the widget

--- turnstile/widgets.py
+++ turnstile/widgets.py
@@ -6,6 +6,17 @@ from django.forms import widgets
 class TurnstileWidget(widgets.Widget):
     template_name = "turnstile/turnstile_widget.html"
 
+    def __init__(self, *args, **kwargs):
+        self.appearance = kwargs.pop("appearance", "always")
+
+        if self.appearance not in ("always", "execute", "interaction-only"):
+            raise ImproperlyConfigured(
+                "Widget appearance must be set to "
+                "'always', 'exeucte', or 'interaction-only'."
+            )
+
+        super().__init__(*args, **kwargs)
+
     def get_context(self, name, value, attrs):
         if not hasattr(settings, "TURNSTILE_SITE_KEY"):
             raise ImproperlyConfigured(
@@ -14,5 +25,6 @@ class TurnstileWidget(widgets.Widget):
 
         return {
             **super().get_context(name, value, attrs),
+            "appearance": self.appearance,
             "site_key": settings.TURNSTILE_SITE_KEY
         }
